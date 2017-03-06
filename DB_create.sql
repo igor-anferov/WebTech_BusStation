dcd1937d8634aeb82591e3a154e09ba631e6094d
igor-anferov <igor-anferov@mail.ru>
2017-03-07 02:09:35 +0300

    First stage looks like done

--- DB_create.sql
+++ DB_create.sql
@@ -30,15 +30,6 @@ CREATE TABLE Runs(
 	BusCapacity INTEGER NOT NULL
 );
 
-CREATE TABLE Orders(
-	`Order` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	Client INTEGER NOT NULL REFERENCES Clients(Client) ON DELETE CASCADE ON UPDATE CASCADE,
-	Run INTEGER NOT NULL REFERENCES Runs(Run) ON DELETE CASCADE ON UPDATE CASCADE,
-    `From` INTEGER NOT NULL REFERENCES Stations(Station) ON DELETE CASCADE ON UPDATE CASCADE,
-    `To` INTEGER NOT NULL REFERENCES Stations(Station) ON DELETE CASCADE ON UPDATE CASCADE,
-    Cost INTEGER NOT NULL
-);
-
 CREATE TABLE Schedule(
 	`Stop` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	Run INTEGER NOT NULL REFERENCES Runs(Run) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -46,15 +37,110 @@ CREATE TABLE Schedule(
 	Arrival DATETIME NULL,
 	Departure DATETIME NULL,
 	
-    UNIQUE (Run, Station, Arrival),
-    UNIQUE (Run, Station, Departure)
+    UNIQUE (Run, Station),
+    
+    CHECK (Departure IS NULL OR Arrival IS NULL OR Departure > Arrival)
 );
 
+DELIMITER //
+CREATE TRIGGER IntersectElimination
+BEFORE INSERT ON Schedule
+FOR EACH ROW
+BEGIN
+	IF EXISTS (
+		SELECT *
+		FROM Schedule s
+		WHERE NEW.Run = s.Run
+			  AND (
+          s.Arrival IS NULL AND NEW.Arrival <= s.Departure OR
+
+          s.Departure IS NULL AND NEW.Departure >= s.Arrival OR
+
+          NEW.Arrival IS NULL AND s.Arrival <= NEW.Departure OR
+
+          NEW.Departure IS NULL AND s.Departure >= NEW.Arrival OR
+
+          s.Arrival IS NOT NULL AND s.Departure IS NOT NULL AND NEW.Arrival IS NOT NULL AND NEW.Departure IS NOT NULL AND
+          (
+				      NEW.Arrival between s.Arrival AND s.Departure OR
+				      s.Arrival between NEW.Arrival AND NEW.Departure
+          )
+			  )
+	) THEN
+		SIGNAL SQLSTATE '11111';
+	END IF;
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER SingleSourceAndDest
+AFTER INSERT ON Schedule
+FOR EACH ROW
+BEGIN
+	IF (
+		NEW.Arrival IS NULL AND (
+			SELECT COUNT(*)
+            FROM Schedule
+            WHERE Arrival IS NULL AND Run = NEW.Run
+        ) != 1 OR
+		NEW.Departure IS NULL AND (
+			SELECT COUNT(*)
+            FROM Schedule
+            WHERE Departure IS NULL AND Run = NEW.Run
+        ) != 1
+    ) THEN
+		SIGNAL SQLSTATE '22222';
+	END IF;
+END//
+DELIMITER ;
+
 CREATE TABLE Costs(
-	Run INTEGER NOT NULL REFERENCES Runs(Run) ON DELETE CASCADE ON UPDATE CASCADE,
-    `From` INTEGER NOT NULL REFERENCES Stations(Station) ON DELETE CASCADE ON UPDATE CASCADE,
-    `To` INTEGER NOT NULL REFERENCES Stations(Station) ON DELETE CASCADE ON UPDATE CASCADE,
-    Cost INTEGER NOT NULL,
+	Cost INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `From` INTEGER NOT NULL REFERENCES Schedule(`Stop`) ON DELETE CASCADE ON UPDATE CASCADE,
+    `To` INTEGER NOT NULL REFERENCES Schedule(`Stop`) ON DELETE CASCADE ON UPDATE CASCADE,
+    Price INTEGER NOT NULL,
     
-	PRIMARY KEY (Run, `From`, `To`)
+    UNIQUE (`From`, `To`)
 );
+
+DELIMITER //
+CREATE TRIGGER SameRuns_AND_ToGraterThanFrom
+AFTER INSERT ON Costs
+FOR EACH ROW
+BEGIN
+	IF NOT EXISTS (
+			SELECT *
+			FROM Schedule f JOIN
+				 Schedule t ON f.Run = t.Run AND
+							   f.Stop = NEW.From AND
+                               t.Stop = NEW.To AND
+							   (f.Arrival IS NULL OR t.Departure IS NULL OR f.Arrival < t.Arrival)
+	) THEN
+		SIGNAL SQLSTATE '33333';
+	END IF;
+END//
+DELIMITER ;
+
+CREATE TABLE Orders(
+	`Order` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	Client INTEGER NOT NULL REFERENCES Clients(Client) ON DELETE CASCADE ON UPDATE CASCADE,
+	Part INTEGER NOT NULL REFERENCES Costs(Cost) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    Count INTEGER NOT NULL DEFAULT 1,
+    Price INTEGER NOT NULL
+);
+
+DELIMITER //
+CREATE TRIGGER DefaultPrice
+BEFORE INSERT ON Orders
+FOR EACH ROW
+BEGIN
+	IF (NEW.Price IS NULL)
+    THEN
+		SET NEW.Price = (
+			SELECT Price
+            FROM Costs
+            WHERE Cost = NEW.Part
+        );
+	END IF;
+END//
+DELIMITER ;
